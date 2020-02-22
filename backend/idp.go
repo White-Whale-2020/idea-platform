daa0cdf15873ddd167dd6f7682af0fd92a8dd1b2
Kyle Thomas <mr.kyle.thomas@gmail.com>
2020-02-22 12:08:09 -0800

    added some url endpoints

--- backend/idp.go
+++ backend/idp.go
@@ -28,13 +28,37 @@ func main() {
 	log.Println("Authors: David Cho, Mike Groper, Kevon Nelson, Kyle Thomas")
 	log.Println("Version: beta")
 
-	http.HandleFunc("/", handler)
-	//http.HandleFunc("/api", )
-	//http.HandleFunc("/api", )
+	http.HandleFunc("/", frontend)
+	http.HandleFunc("/api/", api)
+
+	/* this will serve css and js files later
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	*/
+
 	log.Println("Web server running on port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello!")
+func frontend(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Front end/root url!")
+}
+
+func api(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/api/", "/api/docs":
+		fmt.Fprint(w, "API endpoint!\n")
+		fmt.Fprint(w, "Eventually put api docs here?")
+	case "/api/teams":
+		fmt.Fprint(w, "Teams!")
+	case "/api/users":
+		fmt.Fprint(w, "Users!")
+	case "/api/ideas":
+		fmt.Fprint(w, "ideas!")
+	default:
+		http.NotFound(w, r)
+	}
 }
